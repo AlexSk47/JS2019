25dd5aba6049ba03de57906979ebf49b42967fd5
Alex <skrynnikov.alex@gmail.com>
2019-01-02 22:57:12 +0300

    Первый коммит

---
+++ script.js
@@ -0,0 +1,17 @@
+// var person = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(person.name);
+
+// var arr = ["plum.png", "orange.jpeg", "apple.bmp"];
+// console.log(arr[1]);
+// console.log(typeof(arr[1]));
+
+// alert("Hello world");
+//var answer = confirm("Are you here?");
+//var answer = prompt("Есть ли вам 18?", "Да");
+//console.log(typeof(answer));
+
+console.log(typeof(null));
