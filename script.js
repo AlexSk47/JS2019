d0ee848b16b7b3701caef3c77a27e8f6e00e117c
Alex <skrynnikov.alex@gmail.com>
2019-01-08 14:34:50 +0300

    Задание 4

--- script.js
+++ script.js
@@ -1,22 +1,71 @@
 var money, time;
 
-money = prompt("Ваш бюджет на месяц");
-time = prompt("Введите дату в формате YYYY-MM-DD");
+function start() {
+    money = 50000;
+    time = '';
+
+    while (isNaN(money) || money == '' || money == null) {
+        money = +prompt("Ваш бюджет на месяц", '');
+    }
+}
+
+start();
 
 var appData = {
-    money: money,
-    time: time,
+    budget: money,
+    timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-for (var index = 0; index < 2; index++) {
-    var exp0 = prompt("Введите обязательную статью расходов в этом месяце");
-    var exp1 = prompt("Во сколько обойдется?");
-    appData.expenses[exp0] = exp1;
+function chooseExpenses() {
+    var i = 0;
+    var j = 0;
+    do {
+        if (j >= 5) {
+            console.log('j>5');
+            return;
+        }
+        var a = 's' + i;
+        var b = 0 + i;
+        if (typeof (a) === 'string' && a !== null && b !== null && a.length <= 10) {
+            appData.expenses[a] = b;
+            i++;
+        } else {
+            console.log('Введите строку до 10 символов');
+            j++;
+        }
+    } while (i < 2);
 }
 
-alert(appData.money / 30);
+chooseExpenses();
 
+function detectDayBudget() {
+    return appData.budget / 30;
+}
+
+appData.moneyPerDay = detectDayBudget();
+
+console.log('Ежедневный бюджет: ' + appData.moneyPerDay.toFixed(2));
+
+function checkSavings() {
+    if (appData.savings === true) {
+        var save = 300000;
+        var percent = 10;
+        appData.monthIncome = save / 100 / 12 * percent;
+        console.log('Доход в месяц: ' + appData.monthIncome);
+
+    }
+}
+
+checkSavings();
+
+function chooseOptExpenses() {
+    for (var i = 1; i <= 3; i++) {
+        appData.optionalExpenses[i] = 100 + i;
+    }
+}
+
+chooseOptExpenses();
